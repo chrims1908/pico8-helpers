18a3620496dc1c923ce297811da24d3e133b401f
chrims1908 <chrims1908@gmail.com>
2025-10-21 15:05:26 -0400

    Create boilerplate.lua

---
+++ boilerplate.lua
@@ -0,0 +1,16 @@
+-- your project title
+-- your name
+
+function _init()
+  cls(0)
+  -- poke(0x5f2c, 3) -- 64x64 mode
+end
+
+function _update()
+  
+end
+
+function _draw()
+  cls(0)
+  ?"hello, world!"
+end
